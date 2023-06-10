b4063bfc570daa0645480771decbf38e9b8b988a
Kushal Dube <68413714+kushaldube@users.noreply.github.com>
2023-06-10 23:03:06 +0530

    Create Palindrome.java

---
+++ GFG/Palindrome.java
@@ -0,0 +1,18 @@
+class Solution
+{
+    public String is_palindrome(int n)
+    {
+        // Code here
+        int num=n,x=0;
+        while(n!=0){
+            int r=n%10;
+            x=(x*10)+r;
+            n=n/10;
+        }
+        if(x==num){
+            return "Yes";
+        } else {
+            return "No";
+        }
+    }
+}
