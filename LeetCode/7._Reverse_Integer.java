10d1ad7140423e6397683d407cb7d5bbcd547ce9
Kushal Dube <kushaldube02@gmail.com>
2023-06-13 23:03:43 +0530

    Create 7._Reverse_Integer.java

---
+++ LeetCode/7._Reverse_Integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+        long ans=0;
+
+        while(x!=0){
+            int r=x%10;
+            ans=(ans*10)+r;
+            x=x/10;
+            
+            if(ans<Integer.MIN_VALUE || ans>Integer.MAX_VALUE){
+                return 0;
+            }
+        }g
+        System.out.println(x);
+        System.out.println(ans);
+        return (int)ans;
+
+    }
+}
