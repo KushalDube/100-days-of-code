38e5e84985193c4adc8cda948f2a42bc8f6a5312
Kushal Dube <kushaldube02@gmail.com>
2023-06-22 22:11:18 +0530

    Create 485._Max_Consecutive_Ones.java

---
+++ LeetCode/485._Max_Consecutive_Ones.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count=0, max=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==1){
+                count++;
+                // if(count>max){
+                //     max=count;
+                //     count=0;
+                // }
+            } else {
+                if(count>max){
+                    max=count;
+                }
+                count=0;
+            }
+                    // System.out.print(max+", ");
+        }
+        if(count>max){
+            max=count;
+        }
+        return max;
+    }
+}
