e9b7df9909dd1f8eb72cec05845526a56ac03013
Kushal Dube <kushaldube02@gmail.com>
2023-06-29 22:48:32 +0530

    Create 121._Best_Time_to_Buy_and_Sell_Stock.java

---
+++ LeetCode/121._Best_Time_to_Buy_and_Sell_Stock.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // Approach 1
+        // int max=0,min=0;
+        // for (int i=0; i<prices.length; i++){
+        //     for (int j=i+1; j<prices.length; j++){
+        //         if(j<i){
+        //             min=prices[j];
+        //         }
+        //     }
+        // }
+        // for (int i=0; i<prices.length; i++){
+        //     for (int j=i; j<prices.length; j++){
+        //         if(j>i){
+        //             max=prices[j];
+        //         }
+        //     }
+        // }
+        // System.out.print(max+" "+min);
+        // return max-min;
+
+        
+    }
+}
