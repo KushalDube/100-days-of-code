3e4bee491f1c791226ed3e68a7f99945eb0d7ec9
Kushal Dube <kushaldube02@gmail.com>
2023-07-02 22:19:54 +0530

    Update 326._Power_of_Three.java

--- LeetCode/326._Power_of_Three.java
+++ LeetCode/326._Power_of_Three.java
@@ -6,15 +6,6 @@ class Solution {
         if(n==3 || n==1){
             return true;
         }
-        // int val=0;
-        // for(int i=0; i<n; i++){
-        //     val=(int)Math.pow(3,i);
-        //     // System.out.println(val);
-        //     if(val==n){
-        //         return true;
-        //     }
-        // }
-
         while(n!=1){
             int r=n%3;
             if(r!=0){return false;}
