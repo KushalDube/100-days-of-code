c385057b92757811156f90dc9d04b1b3d031c616
Kushal Dube <kushaldube02@gmail.com>
2023-07-03 23:08:14 +0530

    Update 287._Find_the_Duplicate_Number.java

--- LeetCode/287._Find_the_Duplicate_Number.java
+++ LeetCode/287._Find_the_Duplicate_Number.java
@@ -9,6 +9,19 @@ class Solution {
         //     }
         // }
 
+        // for(int i=0, j=nums.length-1; i<nums.length; ){
+        //     if(nums[i]!=nums[j]){
+        //         j--;
+        //     }
+        //     if(j==i+1){
+        //         i=j;
+        //         j=nums.length-1;
+        //     }
+        //     if(nums[i]==nums[j]) {
+        //         return nums[i];
+        //     }
+        // }
+
         Arrays.sort(nums);
 
         for(int i=1; i<nums.length; i++){
