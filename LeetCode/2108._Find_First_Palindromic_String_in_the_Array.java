bd2a5710268bff11bb77bc5359b3bd3c9b5b524f
Kushal Dube <kushaldube02@gmail.com>
2023-08-13 23:35:16 +0530

    Create 2108._Find_First_Palindromic_String_in_the_Array.java

---
+++ LeetCode/2108._Find_First_Palindromic_String_in_the_Array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPalindrome(String str){
+        StringBuilder sb=new StringBuilder(str);
+        // sb.append(str);
+        String rev=sb.reverse().toString();
+        // for(int i=0; i<str.length(); i++){
+        //     char c=str.charAt(i);
+        //     rev=rev+c;
+        // }
+        if(str.equals(rev)){
+            return true;
+        } else {
+            System.out.print(rev+" ");
+            return false;
+        }
+    }
+
+    public String firstPalindrome(String[] words) {
+        String out="";
+        for(int i=0; i<words.length; i++){
+            if(isPalindrome(words[i])){
+                out=words[i];
+                break;
+            }
+        }
+        return out;
+    }
+}
