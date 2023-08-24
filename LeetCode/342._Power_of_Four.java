07cb379df615d9fac1f99c40a40798e2aa376b6e
Kushal Dube <kushaldube02@gmail.com>
2023-08-24 23:33:42 +0530

    Create 342._Power_of_Four.java

---
+++ LeetCode/342._Power_of_Four.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n<=0) {
+            return false;
+        }
+        if(n==1 || n==4) {
+            return true;
+        }
+
+        while (n>1) {
+            int r=n%4;
+            if(r!=0) {
+                return false;
+            }
+            n/=4;
+        }
+        return true;
+    }
+}
