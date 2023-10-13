d480b075b793cee5a7032ae757c42bb65b00b7e6
Kushal Dube <kushaldube02@gmail.com>
2023-10-13 22:33:28 +0530

    620. Not Boring Movies

--- SQL/620._Not_Boring_Movies.sql
+++ SQL/620._Not_Boring_Movies.sql
@@ -1,5 +1,4 @@
-# Write your MySQL query statement below.
- 
+# Write your MySQL query statement below 
 SELECT * from Cinema
 WHERE mod(id,2) != 0 AND description != "boring"
 ORDER BY rating DESC;
