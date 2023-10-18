2c2f3478b2ba010c541359c55df2bc189cce03be
kushaldube <kushaldube02@gmail.com>
2023-10-19 01:52:40 +0530

    189. Rotate Array

--- LeetCode/189._Rotate_Array.java
+++ LeetCode/189._Rotate_Array.java
@@ -1,9 +1,5 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        //     i=(i+1)%nums.length;
-        // }
-        // System.arraycopy(arr, 0, nums, 0, nums.length);
-
         // logic 
         // place of array: arr[(i+k)%n] = nums[i]; anything divided by the length will return thr remainder within the range of 0 to n-1.
 
